38ecb171631bbb4d20b5ee40722c92b060b5f4f9
Dave Defourneaux <74273226+dd4no@users.noreply.github.com>
2021-08-06 13:32:36 -0400

    Create draggable card and deal button

--- client/src/scenes/game.js
+++ client/src/scenes/game.js
@@ -1,6 +1,5 @@
 export default class Game extends Phaser.Scene {
 
-
  constructor () {
   super({
    key: 'Game'
@@ -15,6 +14,33 @@ export default class Game extends Phaser.Scene {
  }
    
  create () {
+  let self = this;
+
+  this.card = this.add.image(300, 300, 'cyanCardFront').setScale(0.3, 0.3).setInteractive();
+  this.input.setDraggable(this.card);
+
+  this.dealCards = function () {
+
+  }
+
+  this.dealText = this.add.text(75, 350, ['DEAL CARDS']).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff').setInteractive();
+
+  this.dealText.on('pointerdown', function () {
+   self.dealCards();
+  })
+
+  this.dealText.on('pointerover', function () {
+   self.dealText.setColor('#ff69b4');
+  })
+
+  this.dealText.on('pointerout', function () {
+   self.dealText.setColor('#00ffff');
+  })
+
+  this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+   gameObject.x = dragX;
+   gameObject.y = dragY;
+  })
 
  }
 
