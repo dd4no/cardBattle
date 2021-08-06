705c154439b2ec008aa3e1f90ad1cf7781700c03
Dave Defourneaux <74273226+dd4no@users.noreply.github.com>
2021-08-06 13:01:20 -0400

    Clean up template and scaffold new game
    Delete logo,
    Add image assets,
    Create file structure,
    Create game class.

---
+++ client/src/scenes/game.js
@@ -0,0 +1,22 @@
+export default class Game extends Phaser.Scene {
+
+
+ constructor () {
+  super({
+   key: 'Game'
+  });
+ }
+
+ preload () {
+
+ }
+   
+ create () {
+
+ }
+
+ update() {
+
+ }
+
+}
