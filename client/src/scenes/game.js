cc9329a5f88533ef11ff7fa750ab6436cb6bfb77
Dave Defourneaux <74273226+dd4no@users.noreply.github.com>
2021-08-06 13:52:16 -0400

    Create Card class and deal cards

--- client/src/scenes/game.js
+++ client/src/scenes/game.js
@@ -1,51 +1,53 @@
+import Card from '../helpers/card';
+
 export default class Game extends Phaser.Scene {
 
  constructor () {
   super({
    key: 'Game'
   });
- }
+ };
 
  preload () {
   this.load.image('cyanCardFront', 'src/assets/CyanCardFront.png');
   this.load.image('cyanCardBack', 'src/assets/CyanCardBack.png');
   this.load.image('magentaCardFront', 'src/assets/MagentaCardFront.png');
   this.load.image('magentaCardBack', 'src/assets/MagentaCardBack.png');
- }
+ };
    
  create () {
   let self = this;
 
-  this.card = this.add.image(300, 300, 'cyanCardFront').setScale(0.3, 0.3).setInteractive();
-  this.input.setDraggable(this.card);
-
   this.dealCards = function () {
-
-  }
+   for (let i=0; i<5; i++) {
+    let playerCard = new Card(this);
+    playerCard.render(475 + (i*100), 650, 'cyanCardFront')
+   };
+  };
 
   this.dealText = this.add.text(75, 350, ['DEAL CARDS']).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff').setInteractive();
 
   this.dealText.on('pointerdown', function () {
    self.dealCards();
-  })
+  });
 
   this.dealText.on('pointerover', function () {
    self.dealText.setColor('#ff69b4');
-  })
+  });
 
   this.dealText.on('pointerout', function () {
    self.dealText.setColor('#00ffff');
-  })
+  });
 
   this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
    gameObject.x = dragX;
    gameObject.y = dragY;
-  })
+  });
 
- }
+ };
 
  update() {
 
- }
+ };
 
 }
